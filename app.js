1fb2fbc8a6eb235b49f1f5952007807b762a8385
noschwa <nas489@nau.edu>
2024-09-16 19:42:27 -0700

    added mobile support

--- app.js
+++ app.js
@@ -15,41 +15,42 @@ app.listen(3000, () => {
 });
 
 app.post('/', (req, res) => {
-    const { title, author, content } = req.body;
-    const newPost = { id: Date.now(), title, author, content, date: new Date().toLocaleString() };
-    posts.push(newPost);
-    res.redirect('/');
-  });
+  const { title, author, content } = req.body;
+  const newPost = { id: Date.now(), title, author, content, date: new Date().toLocaleString() };
+  posts.push(newPost);
+  res.redirect('/');
+});
 
-  app.post('/edit/:id', (req, res) => {
-    const { title, author, content } = req.body;
-    const post = posts.find(p => p.id == req.params.id);
-    if (post) {
-      post.title = title;
-      post.author = author;
-      post.content = content;
-      post.date = new Date().toLocaleString(); 
-    }
-    res.redirect('/');
-  });
+app.post('/edit/:id', (req, res) => {
+  const { title, author, content } = req.body;
+  const post = posts.find(p => p.id == req.params.id);
+  if (post) {
+    post.title = title;
+    post.author = author;
+    post.content = content;
+    post.date = new Date().toLocaleString();
+  }
+  res.redirect('/');
+});
 
-  app.get('/', (req, res) => {
-    res.render('index', { posts });
-  });
-  
-  app.get('/edit/:id', (req, res) => {
-    const post = posts.find(p => p.id == req.params.id);
-    if (post) {
-      res.render('edit', { post });
-    } else {
-      res.redirect('/');
-    }
-  });
+app.post('/delete/:id', (req, res) => {
+  posts = posts.filter(p => p.id != req.params.id);
+  res.redirect('/');
+});
 
-  app.get('/delete/:id', (req, res) => {
-    posts = posts.filter(p => p.id != req.params.id);
+app.get('/', (req, res) => {
+  res.render('index', { posts });
+});
+
+app.get('/edit/:id', (req, res) => {
+  const post = posts.find(p => p.id == req.params.id);
+  if (post) {
+    res.render('edit', { post });
+  } else {
     res.redirect('/');
-  });
-  
-  
-  
+  }
+});
+
+app.get('/new', (req, res) => {
+  res.render('new');
+});
